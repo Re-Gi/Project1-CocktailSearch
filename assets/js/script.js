5fb73c387e4ed7e1ec2767c6a1746d481997e998
Chris Zavala <frappejcat@gmail.com>
2023-01-13 11:18:43 -0600

    adding more logic to get the search api to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,59 @@
-//Variable declarations: 
+// //Variable declarations: 
 var searchFormEl = document.querySelector('#search-form');
 
 
 function handleSearchFormSubmit(event){
     event.preventDefault();
-    console.log(event);
+    var searchInputVal = document.querySelector('#search-input').value;
+    console.log(searchInputVal);
 
-    var searchInputVal = document.querySelector('#search-input');
-    var formatInputVal = document.querySelector('#drop-down');
+    // var formInputVal = document.querySelector('#drop-box');
 
     if(!searchInputVal){
-        
+        console.log('no search term!');
         return;
     }
-    // var queryString = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita' + searchInputVal + '&format=' + formatInputVal;
-    // location.assign(queryString);
+    searchCocktail(searchInputVal);
 }
+    //This function is getting the DrinkName:
+function searchCocktail(event){
+    event.preventDefault;
+
+
+    var cocktailName = document.querySelector('#search-input').ariaValueMax;
+    console.log(cocktailName);
+    var formatAlcoholicVal = document.querySelector('#drop-down');
+
+
+    
+    let cocktailName = document.querySelector('.search-box');
+    console.log(cocktailName);
+    let drinkAPI = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`;
+
+    fetch(drinkAPI)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            var name = data.drinks[0].strDrink;
+            var img = data.drinks[0].strDrinkThumb;
+            var containAlcohol = data.drinks[0].strAlcoholic;
+            //Need a location to be able to put this date for the name, pic, is drink alcoholic:
+            var officialName = document.querySelector('h2');
+            var cocktailPic = document.querySelector('img');
+            var booleanAlcohol = document.querySelector('h2');
+
+            officialName.innerText = `${name}`;
+            cocktailPic.src = `${img}`;
+            booleanAlcohol.innerText = `${containAlcohol}`;
+            
+
+            // Need to find a location to show information at this point. 
+
+        })
+}
+    
+
 
 //Event Listener
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
