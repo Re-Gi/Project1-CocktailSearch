7929762430f036f8c2febc0d3aa862c06b5955e6
Rebecca Girndt <r.l.girndt@gmail.com>
2023-01-13 12:48:55 -0600

    working on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,46 @@
 //Variable declarations: 
-var searchFormEl = document.querySelector('#search-form');
+// var searchFormEl = document.querySelector('#search-form');
 
 
 function handleSearchFormSubmit(event){
     event.preventDefault();
-    console.log(event);
 
-    var searchInputVal = document.querySelector('#search-input');
-    var formatInputVal = document.querySelector('#drop-down');
+    var searchInputVal = document.querySelector('#search-input').value;
+    console.log(searchInputVal);
+    var formatInputVal = document.querySelector('#drop-down').value;
+    console.log(formatInputVal);
 
     if(!searchInputVal){
-
+        console.log('no search term');
         return;
     }
-    // var queryString = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita' + searchInputVal + '&format=' + formatInputVal;
-    // location.assign(queryString);
+
+    getResults(searchInputVal, formatInputVal);
 }
 
+function getResults(searchInputVal, formatInputVal){
+    console.log(searchInputVal, formatInputVal);
+    // var locQueryUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+//   if (format) {
+//     locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
+//   }
+
+    var locQueryUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchInputVal;
+
+  fetch(locQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        console.log('not ok');
+        throw response.json();
+      }
+
+      console.log(response.json());
+    })
+}
+    
+    // var queryString = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita' + searchInputVal + '&format=' + formatInputVal;
+    // location.assign(queryString);
+
 //Event Listener
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+document.querySelector('#search-form').addEventListener('submit', handleSearchFormSubmit);
