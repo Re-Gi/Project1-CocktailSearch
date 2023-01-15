193224af0305105e71d5d38d40fb746a646f720f
Chris Zavala <frappejcat@gmail.com>
2023-01-14 20:39:29 -0600

    adding more code to the js and html file. Added aligment with the results in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,77 +2,74 @@
 var searchFormEl = document.querySelector('#search-form');
 var resultContentEl = document.querySelector('#result-content');
 var resultTextEl = document.querySelector('#result-text');
+var resultsContainer = document.querySelector("#results");
 //const apiKEY:'7048428';
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
-    // if (!searchInputVal.value) {
-    //     console.log('no search term!');
-
-    //     return;
-    // }
     var searchInputVal = document.querySelector('#search-input').value;
-    // console.log(searchInputVal);
-    // var formatInputVal = document.querySelector('#drop-down').value;
-
+   
     getResults(searchInputVal);
 }
 
 function getResults(searchInputVal) {
     console.log(searchInputVal);
-  
-    var locQueryUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchInputVal;
 
-    fetch(locQueryUrl)
-        .then(function (response) {
-            if (!response.ok) {
-                console.log('not ok');
-                throw response.json();
-            }
+    var URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchInputVal;
 
-            return response.json();
-        })
-        .then(function (locRes) {
-            // resultTextEl.textContent = locRes.search.searchInputVal;
-            console.log(locRes);
-            if (!locRes.drinks){
-                        console.log('No results found!');
-                        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-                    } else {
-                        resultContentEl.textContent = ' ';
-                        for (var i =0; locRes.drinks.strDrink; i++){
-        
-                        }
-                    }
-        
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+            cocktailsData = data.drinks === null ? [] : data.drinks;
+            printResults(cocktailsData);
+            console.log(cocktailsData);
 
-            var drinkname = document.createElement('p');
-            drinkname.textContent = locRes.drinks[0].strDrink;
-            document.getElementById('result-content').append(drinkname);
-
-            var drinkpicture = document.createElement('img');
-            drinkpicture.setAttribute("src", locRes.drinks[0].strDrinkThumb);
-            document.getElementById('result-content').appendChild(drinkpicture);
-            
-        })
-
-        
-        // })
-        // .catch(function(error){
-            // printResults(locRes[i]);
-             //console.log()
-        // });
+        });
+}    
+//need to get a way to print some search.
+//And also made the images clickable. 
+function printResults(cocktails) {
+    resultContentEl.innerHTML = "";
+    for (const element of cocktails) {
+        var list = `<div id="${element.idDrink}" class="cocktail-card"><div id='drinkTitle'><p class="nameCocktail">${element.strDrink}<a><div class="imgWrapper"><img src="${element.strDrinkThumb}"></div></a>`;
+        resultContentEl.innerHTML += list;
+        console.log(cocktails);
+    }    
 }
 
-// function printResults(resultObj){
-//     console.log(resultObj);
-
-//     var resultCard = document.createElement('div');
-//     resultCard.classList.add('card', "bg-light", 'text-dark', "mb-3", 'p-3');
 
 
-// }
+// const searchFormInput = document.querySelector("#search-form");
+// searchFormInput.addEventListener("submit", () =>{
+//     cocktailsData = [];
+//     getResults(URL + searchFormInput.value);
+//     checkError();
+// });
 
+function checkError(){
+    setTimeout(() =>{
+        if(cocktailsData.length === 0){
+        resultContentEl.innerHTML = `<div class=Error<h2>Error Not Found!</h2></div>`;
+        resultsContainer.innerHTML = "";
+    } else {
+        printResults(cocktailsData);
+    }
+
+    }, 1000);
+    resultContentEl.innerHTML = "";
+
+}
+//this will open the drink item to the next page. 
+function clickResults() {
+    document.querySelectorAll("#results").forEach((item) =>{
+        item.addEventListener("click", () => openDrink(item));
+    });
+}
+//takes us to the future html 
+function openDrink(element) {
+    localStorage.setItem("id", element.id);
+    window.open("./future.html");
+}
 
 //Event Listener
 document.querySelector('#search-form').addEventListener('submit', handleSearchFormSubmit);
@@ -96,44 +93,62 @@ document.querySelector('#search-form').addEventListener('submit', handleSearchFo
 
 
 
-//     searchCocktail(searchInputVal);
-// }
-//     //This function is getting the DrinkName:
-//     function searchCocktail(event){
-//     event.preventDefault;
-
-
-//     var cocktailName = document.querySelector('#search-input').ariaValueMax;
-//     console.log(cocktailName);
-//     var formatAlcoholicVal = document.querySelector('#drop-down');
 
 
 
-//     let cocktailName = document.querySelector('.search-box');
-//     console.log(cocktailName);
-//     let drinkAPI = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`;
-
-//     fetch(drinkAPI)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data)
-
-//             var name = data.drinks[0].strDrink;
-//             var img = data.drinks[0].strDrinkThumb;
-//             var containAlcohol = data.drinks[0].strAlcoholic;
-//             //Need a location to be able to put this date for the name, pic, is drink alcoholic:
-//             var officialName = document.querySelector('h2');
-//             var cocktailPic = document.querySelector('img');
-//             var booleanAlcohol = document.querySelector('h2');
-
-//             officialName.innerText = `${name}`;
-//             cocktailPic.src = `${img}`;
-//             booleanAlcohol.innerText = `${containAlcohol}`;
 
 
-//             // Need to find a location to show information at this point. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 console.log('not ok');
+//                 throw response.json();
+//             }
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+
+//             if (!data) {
+//                 console.log('No results found!');
+//                 resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+//             } else {
+//                 resultContentEl.textContent = ' ';
+//                 for (var i = 0; data.drinks.strDrink; i++) {
+//                     printResults(lockRes[i].strDrink);
+//                 }
+//             }
+
+
+//             var drinkname = document.createElement('p');
+//             drinkname.textContent = data.drinks[i].strDrink;
+//             document.getElementById('result-content').append(drinkname);
+
+//             var drinkpicture = document.createElement('img');
+//             drinkpicture.setAttribute("src", data.drinks[i].strDrinkThumb);
+//             document.getElementById('result-content').appendChild(drinkpicture);
 
 //         })
+        
+// }
+
 
 
 
